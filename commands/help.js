d48ed7bb031c030ebd564b9bb3e5bd2974773c16
mskhh0505 <66984236+mskhh0505@users.noreply.github.com>
2020-08-27 11:36:35 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,12 @@ const prefix = ";";
 exports.run = async (client, message, args) => {
     return message.channel.send({embed: {
         color: 7948427,
-        description: `**명령어 목록 입니다.**\n`
-        + `\`${prefix}help\` - 명령어 목록을 보여줍니다.\n`
-        + `\`${prefix}setrank <유저> <랭크 이름/숫자>\` - 유저를 랭크 이름 또는 숫자로 설정합니다.\n`
-        + `\`${prefix}clearshout\` - 그룹의 샤우트를 클리어합니다.\n`
-        + `\`${prefix}accept-join <user>\` - 그룹요청을 받습니다.\n`
-        + `\`${prefix}deny-join <user>\` - 그룹요청을 거절 합니다.`,
+        description: `**명령어 목록**\n`
+        + `\`${prefix}명령어\` - 명령어 사용법과 목록을 보여줍니다.\n`
+        + `\`${prefix}수락 <유저>\` - 그룹 가입 요청을 명령어로 통해 받을 수 있습니다.\n`
+        + `\`${prefix}진급 <유저>\` - 유저의 7여단 그룹 계급을 진급 시킬 수 있습니다..\n`
+        + `\`${prefix}강등 <유저>\` - 유저의 7여단 그룹 계급을 강등 시킬 수 있습니다..\n`
+        + `\`${prefix}정보 <유저>\` - 유저의 정보를 확인 할 수 있습니다.`,
         author: {
             name: message.author.tag,
             icon_url: message.author.displayAvatarURL()
